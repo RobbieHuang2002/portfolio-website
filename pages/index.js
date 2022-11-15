34be8e3329d2ee8b448735d36593b14117d4f077
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-11-14 19:15:19 -0500

    added dark mode

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <section className='min-h-screen'>
           {/*padding, margin-bottom, flex, justify*/}
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons'>DevelopedbyRob</h1>
+            <h1 className='text-xl font-burtons dark:text-white'>DevelopedbyRob</h1>
             <ul className='flex itmes-center'>
               <li>
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/>
@@ -34,13 +34,13 @@ export default function Home() {
           </nav>
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Robbie Huang</h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>Developer</h3>
-            <p className='text-medium py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>
+            <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Developer</h3>
+            <p className='text-medium py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white'>
               Computer Science student at Queen's University in 3rd year.
               Enjoys machine learning and web developement!
             </p>
           </div>
-          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white'>
             <a href='https://github.com/RobbieHuang2002'>
               <AiFillGithub />
             </a>
@@ -56,15 +56,15 @@ export default function Home() {
         <section>
           <div className='text-center'>
             {/*projects title */}
-            <h3 className='text-3xl py-1'>Projects I've built</h3>
-            <p className='text-md py-2 leading-8 text-gray-80'>
+            <h3 className='text-3xl py-1 dark:text-white'>Projects I've built</h3>
+            <p className='text-md py-2 leading-8 text-gray-80 dark:text-white'>
               Since the beggining of my journey as a developer, I've built different apps! 
             </p>
           </div>
           {/*project cards*/}
           <div className='lg:flex justify-center gap-10'>
             {/*first project card */}
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={hackathon} width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Chef Buddy</h3>
               <p className='py-2 max-w-xs'>
@@ -79,7 +79,7 @@ export default function Home() {
               </a>
             </div>
             {/*second project card */}
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={movie} width={100} height={100}/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Movie Review NLP </h3>
               <p className='py-2 max-w-xs'>
@@ -94,16 +94,16 @@ export default function Home() {
               </a>
             </div>
             {/*third project card*/}
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={instagram} width={100} height={100}/>
-              <h3 className='text-lg font-medium pt-8 pb-2'></h3>
-              <p className='py-2'>
-                creating elegant deisng suited for your needs 
+              <h3 className='text-lg font-medium pt-8 pb-2'>Organic Post Prediction</h3>
+              <p className='py-2 max-w-xs'>
+                 Using image classification to determine which instagram photo will perform the best
               </p>
               <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-              <p className='text-gray-800 py-1'>photoshop</p>
-              <p className='text-gray-800 py-1'>figma</p>
-              <p className='text-gray-800 py-1'>illustrator</p>
+              <p className='text-gray-800 py-1'>Keras</p>
+              <p className='text-gray-800 py-1'>Pandas</p>
+              <p className='text-gray-800 py-1'>Numpy</p>
             </div>
           </div>
         </section>
